fb174e31061803eb2de2f2902c2c8656c3ec507f
qwer45us <agni.tripathi@kellogg.northwestern.edu>
2022-05-16 14:56:55 +0000

    v1

---
+++ app/controllers/place_controller.rb
@@ -0,0 +1,21 @@
+class PlaceController < ApplicationController
+    def index
+        @places = Place.all
+      end
+    
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+        @posts = Contact.where({ "place_id" => @places["id"] })
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Post.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+end
